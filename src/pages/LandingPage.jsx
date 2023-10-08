f201f9ad36072f269704ae407d59d5da3897a161
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-10-08 17:39:41 +0530

    improved button styling

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,8 @@
-import { Button, Flex, Heading } from '@chakra-ui/react'
+import { Button, Flex, Heading, useTheme } from '@chakra-ui/react'
 import { useNavigate } from 'react-router-dom'
 
 const LandingPage = () => {
+    const theme = useTheme()
     const navigate = useNavigate()
     return (
         <Flex flexDir="column" alignItems="center" gap="2rem" justifyContent="center" height="100vh">
@@ -9,7 +10,22 @@ const LandingPage = () => {
                 <Heading>Please Sign In. below</Heading>
                 <Heading>to continue</Heading>
             </Flex>
-            <Button onClick={() => navigate("/login")}>Go to SignIn</Button>
+            <Button onClick={() => navigate("/login")}
+              gap="0.5rem"
+              borderRadius="20px"
+              transition={"all 0.3s ease"}
+              color={theme.colors.button.dark_color}
+              backgroundColor={theme.colors.button.dark_backgroundColor}
+              _hover={{
+                backgroundColor: `${theme.colors.button.hover_dark_backgroundColor}`,
+                transform: `${theme.colors.button.hover_transform}`,
+              }}
+              _active={{
+                backgroundColor: `${theme.colors.button.active_dark_backgroundColor}`,
+              }}
+            >
+              Go to SignIn
+            </Button>
         </Flex>
     )
 }
